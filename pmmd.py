128cb49133693440d81ed60f2e153213404997ca
hengsgg <dnflekf2748@gmail.com>
2022-06-18 19:35:21 +0900

    python and javascript learn

---
+++ pmmd.py
@@ -0,0 +1,7 @@
+import math
+A,B = input().split()
+print(math.trunc(int(A)+int(B)))
+print(math.trunc(int(A)-int(B)))
+print(math.trunc(int(A)*int(B)))
+print(math.trunc(int(A)/int(B)))
+print(math.trunc(int(A)%int(B)))
